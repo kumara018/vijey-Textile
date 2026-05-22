89cf8c606a22d3dde52fc4a8cd209365654b215a
Kumaraguru <kumaraguru27102@gmail.com>
2026-05-22 20:35:50 +0530

    Update favicon: crown + princess dress matching new logo style, force browser refresh with v2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -53,8 +53,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.svg" />
-        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+        <link rel="shortcut icon" href="/favicon.svg?v=2" />
+        <link rel="apple-touch-icon" href="/favicon.svg?v=2" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
